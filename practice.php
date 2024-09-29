93a88814f1df5d6615adc2251229121b5763427f
yuria <aw0asgt@gmail.com>
2024-09-29 15:37:57 +0000

    PHP09課題完成①

--- practice.php
+++ practice.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*1. 「http://◯◯◯◯.jp/XXX というアクセスが来たときに、 
+/*「http://◯◯◯◯.jp/XXX というアクセスが来たときに、 
 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください*/
 
 Route::get('XXX','AAAController@bbb');
