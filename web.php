edbbde17069049b27ce0b2e1c8ed4832e7c906cd
yuria <aw0asgt@gmail.com>
2024-09-29 15:30:37 +0000

    PHP09課題完成②

--- web.php
+++ web.php
@@ -21,7 +21,9 @@ use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function(){
     Route::get('news/create', 'add');
 });
-
+/*【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
+web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
+admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください*/
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function(){
     Route::get('profile/create','add');
